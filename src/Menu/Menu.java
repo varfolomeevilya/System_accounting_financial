03b55c62de7d10de511e0d214e7aaab69c420552
varfolomeevilya <varfolomeevi83@gmail.com>
2023-11-15 19:26:11 +0300

    option4

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -17,6 +17,7 @@ public class Menu {
 
     public Menu() {
     }
+
     public Menu(Wallet wallet, Current_costs current_costs, Potential_costs potential_costs, Potential_income potential_income) {
         this.wallet = wallet;
         this.current_costs = current_costs;
@@ -428,7 +429,7 @@ public class Menu {
                 if (menu08() == 8) {
                     current_costs.getStr();
                 } else {
-                    System.out.println(" Работа со списком текущих затрат\n");
+                    System.out.println("получения общей суммы по всем кошелькам и картам");
                 }
 
             }
@@ -436,7 +437,7 @@ public class Menu {
                 if (menu09() == 9) {
                     potential_costs.setPrice(1.215000);
                 } else {
-                    System.out.println(" Работа со списком потенциальный затрат\n");
+                    System.out.println("9 Работа со списком текущих затрат\n");
                 }
 
             }
@@ -467,31 +468,38 @@ public class Menu {
 
 
     public int menu01() {
-        if (menu01()==1) {
-            System.out.println("Открыть кошелек");
+        if (menu02() == 2) {
+            wallet.allSumma();
+            System.out.println("Получение деньги кошелька");
         }
-    onWallet1(scanner.nextInt());
+//        else {
+//            System.out.println("Получение деньги кошелька");
+//        }
+        onWallet2(scanner.nextInt());
         return menu01();
     }
 
     public int menu02() {
-        if (menu02() == 2) {
-            wallet.allSumma();
-        } else {
-            System.out.println("2: Получение деньги по всем кошелькам\n");
+        if (menu01() == 1) {
+            System.out.println("Открыть кошелек");
         }
-        onWallet2(scanner.nextInt());
+//        else{
+//
+//        }
+        onWallet1(scanner.nextInt());
 
         return menu02();
     }
 
     public int menu03() {
 
-        if (menu03() == 3) {
-            wallet.addCheques(3100);
-        } else {
-            System.out.println("Добавление деньги в кошелька");
-        }
+        if (menu04() == 4) {
+            wallet.deleteCheques(300);
+            System.out.println("Удаление деньги  кошелька");
+       }
+//        else {
+//            System.out.println("Удаление деньги  кошелька");
+//        }
 
         onWallet3(scanner.nextInt());
 
@@ -499,86 +507,106 @@ public class Menu {
     }
 
     public int menu04() {
-        if (menu04() == 4) {
-            wallet.deleteCheques(300);
-        } else {
-            System.out.println("Удаление деньги  кошелька");
+        if (menu03() == 3) {
+            wallet.addCheques(3100);
+            System.out.println("Добавление деньги в кошелька");
         }
-        onWallet1(scanner.nextInt());
+//        else {
+//            System.out.println("Добавление деньги в кошелька");
+//        }
+
+        onWallet2(scanner.nextInt());
         return menu04();
     }
 
     public int menu05() {
-        if (menu05() == 5) {
-            System.out.println("Открыть кредитную карту");
+        if (menu06()==6) {
+            wallet.addCredit_cards("Visa", 25000);
+            System.out.println("Добавить редактирование информацию кредитной карты");
         }
-        onWallet2(scanner.nextInt());
+//        else {
+//            System.out.println("Добавить редактирование информацию кредитной карты");
+//        }
+        onWallet3(scanner.nextInt());
         return menu05();
     }
 
+
     public int menu06() {
-        if (menu06() == 6) {
-            wallet.addCredit_cards("Visa", 25000);
-        } else {
-            System.out.println("Добавить редактирование информацию кредитной карты");
+        if (menu05() == 5) {
+            System.out.println("Открыть кредитную карту");
         }
-        onWallet3(scanner.nextInt());
+//        else{
+//
+//        }
+        onWallet2(scanner.nextInt());
         return menu06();
     }
 
     public int menu07() {
-        if (menu07() == 7) {
-            wallet.deleteCredit_cards("Mastercard", 50000);
-        } else {
-            System.out.println("Удаление  кредитной карты");
+        if (menu08() == 8) {
+           wallet.credit_cardsSumma();
+            System.out.println("8:получения общей суммы по всем кошелькам и картам\n");
         }
-        onWallet2(scanner.nextInt());
+//        else{
+//            System.out.println("8:получения общей суммы по всем кошелькам и картам\n");
+//        }
+        onWallet3(scanner.nextInt());
         return menu07();
     }
 
     public int menu08() {
-        if (menu08() == 8) {
-            current_costs.getStr();
-        } else {
-            System.out.println(" Работа со списком текущих затрат\n");
+        if (menu07() == 7) {
+            wallet.deleteCredit_cards("Mastercard", 50000);
+            System.out.println("7Удаление  кредитной карты");
         }
-        onWallet3(scanner.nextInt());
+//        else{
+//            System.out.println("7Удаление  кредитной карты");
+//        }
+        onWallet2(scanner.nextInt());
         return menu08();
     }
 
     public int menu09() {
-        if (menu09() == 9) {
-            potential_costs.setPrice(1.215000);
-        } else {
-            System.out.println(" Работа со списком потенциальный затрат\n");
+        if (menu10() == 10) {
+            potential_income.getPrize();
+
+            potential_income.getSalary();
+            System.out.println(" Работа со списком потенциальный доход");
         }
+//        else{
+//            System.out.println(" Работа со списком потенциальный доход");
+//        }
         onWallet3(scanner.nextInt());
         return menu09();
     }
 
     public int menu10() {
-        if (menu10() == 10) {
-            potential_income.setPrize(10);
-
-            potential_income.setSalary(15000);
-        } else {
+        if (menu11() == 11) {
+            potential_income.getPrize();
+            potential_income.getSalary();
             System.out.println(" Работа со списком потенциальный доход");
         }
-        onWallet3(scanner.nextInt());
+//        else{
+//            System.out.println(" Работа со списком потенциальный доход");
+//        }
+        onWallet2(scanner.nextInt());
         return menu10();
     }
 
     public int menu11() {
-
-        if (menu11() == 11) {
-            potential_income.getPrize();
-            potential_income.getSalary();
-        } else {
-            System.out.println(" Работа со списком потенциальный доход");
+       if (menu09() == 9) {
+            potential_costs.getStr();
+            potential_costs.getPrice();
+           System.out.println(" Работа со списком потенциальный затрат\n");
         }
-        onWallet3(scanner.nextInt());
+//        else {
+//           System.out.println(" Работа со списком потенциальный затрат\n");
+//         }
+        onWallet2(scanner.nextInt());
         return menu11();
     }
+
     @Override
     public String toString() {
         return "Menu{" +
